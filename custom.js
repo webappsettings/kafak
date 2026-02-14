010ae764b85fe59e62e425a6c84fa119b4ff0abd
webappsettings <webappsettings@gmail.com>
2026-02-14 11:12:35 +0530

    code update

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 // ------------------------------------------------------------------------------
 // 🔴 CONFIGURATION & GLOBALS
 // ------------------------------------------------------------------------------
-const sc = `https://script.google.com/macros/s/AKfycbwrzHhim7IJi5WSfv_5BfNm7e2qL6tsrqFJ4DXfvf1b-KyqqV5BGgOpgLO4stkFVKHzEQ/exec`;
+const sc = `https://script.google.com/macros/s/AKfycbyy8cXH_lp-QwCa7REs1ZrwACWXLgfTbernGr-YT5ScNJZvMYfS5Xl1q7brUW0ALzDPXQ/exec`;
 
 let currentStep = 0;
 let editingOrderId = null;
