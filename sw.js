cff18b8fc0b4d48fa45fa07063f445ddd16dcfe4
webappsettings <webappsettings@gmail.com>
2026-02-20 10:41:27 +0530

    to app

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'kafak-app-v1';
+
+// കാഷെ ചെയ്യേണ്ട പ്രധാന ഫയലുകൾ
+const urlsToCache = [
+    './order.html',
+    './admin.html',
+    './custom.js',
+    './admin.js',
+    './translations.js',
+    './logo.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// ഇന്റർനെറ്റ് ഉണ്ടെങ്കിൽ പുതിയ ഡാറ്റ എടുക്കും, അല്ലെങ്കിൽ പഴയ സേവ് ചെയ്ത ഡാറ്റ കാണിക്കും
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
