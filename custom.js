fb8515f4602af2bb7321c563d55aae5f346b6b59
webappsettings <webappsettings@gmail.com>
2026-02-21 11:54:44 +0530

    err fix

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 ﻿// ------------------------------------------------------------------------------
 // 🔴 CONFIGURATION & GLOBALS
 // ------------------------------------------------------------------------------
-const sc = `https://script.google.com/macros/s/AKfycbyFRRLEE33M-iboIhYEQH6LK75xImHQ14zab0d9WMHCLS9JA9_NrtsyOChVALl5H3VR/exec`;
+const sc = `https://script.google.com/macros/s/AKfycbwNhN8d6meTyuOmIodsvHhSkJHudxyt112Pli4jMRVrN1tGoOIsXy3BSp6M4OGhZY2Kxw/exec`;
 // ------------------------------------------------------------------------------
 // 🔴 CONFIGURATION & GLOBALS
 
