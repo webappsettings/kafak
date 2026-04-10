8dc076f1ccab600f524fb5bdbd1663d2ee2e34dc
webappsettings <webappsettings@gmail.com>
2026-04-10 18:11:39 +0530

    print a6 updated

--- admin.js
+++ admin.js
@@ -3030,13 +3030,15 @@ async function runPrintLogic(checkboxes, directData = null) {
             printCourierBorder = "1px solid #dc2626";
         }
 
+
+
         let trackingBarcodeHtml = '';
         if (d.tracking) {
-            let barcodeImg = item.trackBarcodeSrc ? `<img src="${item.trackBarcodeSrc}" style="width:47mm; height:7.2mm; display:block; margin:0 auto;" />` : '';
+            let barcodeImg = item.trackBarcodeSrc ? `<img src="${item.trackBarcodeSrc}" style="width: 48mm;height: 7.4mm;display:block;margin:0 auto;" />` : '';
 
             trackingBarcodeHtml = `
-            <div style="position:absolute; top:4mm; right:4mm; text-align:center; z-index:50; background-color: #ffffff; border: 1.5px solid #cbd5e1; border-radius: 6px; padding: 5px; min-width: 50mm;">
-                <img src="images/indiapostlogo.svg" style="position:absolute; top:2px; right:5px; height:14px;" alt="indiapostlogo" />
+            <div style="position:absolute;top:4mm;right:4mm;text-align:center;z-index:50;background-color: #ffffff;border: 1.5px solid #cbd5e1;border-radius: 6px;padding: 4px 10px;min-width: 50mm;">
+                <img src="images/indiapostlogo.svg" style="position:absolute;top: -7px;right: 7px;height: 18px;" alt="indiapostlogo" />
                 <div style="font-size: 7px; font-weight: 800; color: #64748b; text-transform: uppercase; letter-spacing: 0.5px; margin-bottom: 3px; text-align:left; padding:0 5px;">Consignment ID</div>
                 ${barcodeImg}
                 <div style="font-size:11px; font-weight:900; letter-spacing:1px; font-family:monospace; color:#000; margin-top:2px;">${d.tracking}</div>
